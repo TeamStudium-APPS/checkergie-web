3386c7bec4038ce397a1e288f62ab4b84473b227
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-09-21 16:44:37 +0900

    refactor: axios 기본 content-type 헤더 제거

--- packages/api/src/client/axios-client.ts
+++ packages/api/src/client/axios-client.ts
@@ -5,7 +5,4 @@ const REQUEST_TIMEOUT = 10 * 1000;
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: REQUEST_TIMEOUT,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
